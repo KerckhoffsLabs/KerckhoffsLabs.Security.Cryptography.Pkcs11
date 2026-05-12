77ec2dfa8e2bb735c7783ecd49b34853d2070cb7
Alexandre Laroche <larochealexandre@hotmail.com>
2026-05-12 12:45:38 -0400

    refactor(Session): carve DeriveKey into Session.Derive.cs
    Pure relocation. Sets up T7 (DeriveSharedSecretEcdh helper).

--- src/KerckhoffsLabs.Security.Cryptography.Pkcs11/HighLevel/Session.cs
+++ src/KerckhoffsLabs.Security.Cryptography.Pkcs11/HighLevel/Session.cs
@@ -389,46 +389,6 @@ public partial class Session
     }
 
     /// <summary>
-    /// Derives a key from a base key, creating a new key object
-    /// </summary>
-    /// <param name="mechanism">Derivation mechanism</param>
-    /// <param name="baseKeyHandle">Handle of base key</param>
-    /// <param name="attributes">Attributes for the new key</param>
-    /// <returns>Handle of derived key</returns>
-    public ObjectHandle DeriveKey(Mechanism mechanism, ObjectHandle baseKeyHandle, List<ObjectAttribute> attributes)
-    {
-        if (_disposed)
-            throw new ObjectDisposedException(GetType().FullName);
-
-        _logger.Debug("Session({0})::DeriveKey", _sessionId);
-
-        if (mechanism == null)
-            throw new ArgumentNullException("mechanism");
-        
-        if (baseKeyHandle == null)
-            throw new ArgumentNullException("baseKeyHandle");
-
-        CK_MECHANISM ckMechanism = (CK_MECHANISM)mechanism.ToMarshalableStructure();
-
-        CK_ATTRIBUTE[] template = null;
-        NativeCULong templateLen = (NativeCULong)0;
-        if (attributes != null)
-        {
-            template = new CK_ATTRIBUTE[attributes.Count];
-            for (int i = 0; i < attributes.Count; i++)
-                template[i] = attributes[i].CkAttribute;
-            templateLen = (NativeCULong)(attributes.Count);
-        }
-
-        NativeCULong derivedKey = CK.CK_INVALID_HANDLE;
-        CKR rv = _pkcs11Library.C_DeriveKey(_sessionId, ref ckMechanism, (NativeCULong)(baseKeyHandle.ObjectId), template, templateLen, ref derivedKey);
-        if (rv != CKR.CKR_OK)
-            throw new Pkcs11Exception("C_DeriveKey", rv);
-
-        return new ObjectHandle((ulong)derivedKey);
-    }
-
-    /// <summary>
     /// Legacy function which should throw CKR_FUNCTION_NOT_PARALLEL
     /// </summary>
     public void GetFunctionStatus()
